0f621af15dfa9bd984b96f1f9fbc57e69fb7835d
unknown <you@example.com>
2022-10-01 14:42:08 +0530

    mouse hover

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ var paddleWidth = 65;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
-var brickRowCount = 7;
+var brickRowCount = 8;
 var brickColumnCount = 4;
-var brickWidth = 65;
-var brickHeight = 15;
-var brickPadding = 10;
+var brickWidth = 45;
+var brickHeight = 10;
+var brickPadding = 8;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 var score = 0;
@@ -40,7 +40,7 @@ btn.addEventListener('click', () => {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-// document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
@@ -60,12 +60,12 @@ function keyUpHandler(e) {
     }
 }
 
-// function mouseMoveHandler(e) {
-//   var relativeX = e.clientX - canvas.offsetLeft;
-//   if(relativeX > 0 && relativeX < canvas.width) {
-//     paddleX = relativeX - paddleWidth/2;
-//   }
-// }
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if(relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth/2;
+  }
+}
 function collisionDetection() {
   for(var c=0; c<brickColumnCount; c++) {
     for(var r=0; r<brickRowCount; r++) {
